e4e3bc4093bc8977d0a73999b5d46f7b38826ff4
njagua <njagua.gma@gmail.com>
2024-07-02 06:05:55 +0300

    adds index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// anonymous functions
+const sum = function(){
+return x + y;
+}
+console.log(1 + 1)
+
+const difference = function(){
+    return x - y;
+}
+console.log(10 - 1)
+
+const foo = function(){
+    return bar
+}
+console.log(foo)
+
+//ARROW FUNCTIONS TO DECLARE A FUNCTION
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
+
+//ASSIGNMENT
+const divide =  function(){
+    return 2000 / 100;
+}
+console.log(divide());
+
+const square = (h) => h * h;
+console.log(square(7))
